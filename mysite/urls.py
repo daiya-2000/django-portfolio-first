6e0223378aa1fdb76cecfc830d9363e793c79840
佐藤　大弥 <81219779+daiya-2000@users.noreply.github.com>
2021-06-07 01:46:06 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -26,3 +28,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
